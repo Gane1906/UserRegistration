f7ec889fd7faa7e98a531049b8a08c3d81e7e8b3
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-03-28 12:19:59 +0530

    [Ganesh] Add:User registration file added

---
+++ UserRegistration/Program.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace UserRegistration
+{
+    public class Program
+    {
+        static void Main(String[] args)
+        {
+            Console.WriteLine("Welcome to user Registration!");
+        }
+    }
+}
