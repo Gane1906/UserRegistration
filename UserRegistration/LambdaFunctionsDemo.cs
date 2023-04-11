e540a1bbe87f7aabc71c3dfba76349af7095ad45
Ganesh Doppani <ganesh.doppani@gmail.com>
2023-04-11 14:15:15 +0530

    [Ganesh] Add:Lambda functions for user details is added

---
+++ UserRegistration/LambdaFunctionsDemo.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UserRegistration
+{
+    public class LambdaFunctionsDemo
+    {
+        public bool ValidateFirstName() => Regex.IsMatch(Console.ReadLine(), "^[A-Z]{1}[a-z]{2,}$");
+        public bool ValidateLastName() => Regex.IsMatch(Console.ReadLine(), "^[A-Z]{1}[a-z]{2,}$");
+        public bool ValidateEmail() => Regex.IsMatch(Console.ReadLine(), "^[0-9A-Za-z]+[.+-_]{0,1}[0-9A-Za-z]+[@][]A-Za-z]+[.][a-z]{2,3}([.][a-z]{2,3}){0,1}$");
+        public bool ValidatePhone() => Regex.IsMatch(Console.ReadLine(), "^[6-9]{1}[0-9]{9}$");
+        public bool ValidatePassword() => Regex.IsMatch(Console.ReadLine(), "^(?=[a-zA-Z0-9#@$?-_]{8,}$)(?=.*?[a-z])(?=.*?[A-Z])(?=.*?[0-9]).*$");
+    }
+}
--- UserRegistration/Program.cs
+++ UserRegistration/Program.cs
@@ -7,8 +7,32 @@ namespace UserRegistration
     {
         static void Main(String[] args)
         {
-            UserValidation userValidation = new UserValidation();
-            Console.WriteLine(userValidation.ValidateFirstName("G"));
+            LambdaFunctionsDemo lambda = new LambdaFunctionsDemo();
+            Console.Write("Enter First name: ");
+            if (lambda.ValidateFirstName())
+                Console.WriteLine("Valid First name");
+            else
+                Console.WriteLine("Invalid First name");
+            Console.Write("Enter Last name: ");
+            if (lambda.ValidateLastName())
+                Console.WriteLine("Valid Last name");
+            else
+                Console.WriteLine("Invalid Last name");
+            Console.Write("Enter Email: ");
+            if (lambda.ValidateEmail())
+                Console.WriteLine("Valid email");
+            else
+                Console.WriteLine("Invalid email");
+            Console.Write("Enter Phone number: ");
+            if (lambda.ValidatePhone())
+                Console.WriteLine("Valid mobile number");
+            else
+                Console.WriteLine("Invalid mobile number");
+            Console.Write("Enter Password: ");
+            if (lambda.ValidatePassword())
+                Console.WriteLine("Valid Password");
+            else
+                Console.WriteLine("Invalid Password");
         }
     }
 }
